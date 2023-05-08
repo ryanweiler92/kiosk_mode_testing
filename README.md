24328b1f429fb4c8debeb770951bc1a2c18f5773
ryanweiler92 <ryanweiler92@gmail.com>
2023-05-08 13:50:46 -0400

    local & SIT urls

---
+++ README.md
@@ -0,0 +1,27 @@
+# Browser Automation for Kiosk Mode Testing
+
+This repository contains a Python script to open four instances of Google Chrome, load specified URLs, display them in measured quadrants of your screen, and refresh every 90 seconds. The script is designed to test the behavior of our predefined kiosk mode scenarios in an environment similar to which they will be displayed at EIC. 
+
+## Prerequisites
+
+To run this script, you need to have the following installed on your system:
+
+- Python 3 (preferably the latest version)
+- Google Chrome
+
+## Setup
+
+1. Clone this repository to your local machine
+2. CD into the repository folder
+3. Create the virtual environment with the command: `python3 -m venv venv`
+4. Activate the virtual environment with the command `source venv/bin/activate` (Mac/Linux) or `venv\Scripts\activate` (Windows)
+5. Install the required Python packages using the following command in your terminal: `pip install -r requirements.txt`
+
+## Usage
+
+1. Open the `kiosk_testing.py` file and modify the variables in the url array to include the scenarios you would like to test. Only 4 scenarios can be tested at a single time. 
+2. Run the script using the following command in your terminal: `python browser_automation.py`
+
+The script will open four instances of Google Chrome, each displaying one of the specified URLs. The pages will refresh every 90 seconds.
+
+The SIT urls will require for you to be on the VPN. Alternatively, you can clone the Worldview repository and run the development branch locally and use the local urls. 
---
+++ kiosk_testing.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+
+scenario_1_local = 'http://localhost:3000/?v=-250.17116762774398,-114.67919463709383,250.62557403459047,109.37518092954436&df=true&kiosk=true&l=Coastlines_15m,OrbitTracks_Terra_Descending,MODIS_Terra_CorrectedReflectance_TrueColor&lg=true'
+scenario_2_local = 'http://localhost:3000/?v=-245.94859690671547,-117.45443360228612,249.20837839391078,104.0767339931503&df=true&kiosk=true&l=Coastlines_15m(opacity=0.63),VIIRS_SNPP_DayNightBand_At_Sensor_Radiance&lg=true'
+scenario_3_local = 'http://localhost:3000/?v=-223.38671531740192,-111.23416482365595,252.6798741684782,101.75604370153731&z=4&ics=true&ici=5&icd=10&kiosk=true&df=true&eic=sa&l=Reference_Features_15m,GOES-West_ABI_GeoColor,GOES-East_ABI_GeoColor,VIIRS_SNPP_CorrectedReflectance_TrueColor(opacity=0.61)&lg=true'
+scenario_4_local = 'http://localhost:3000/?v=-250.11417226328018,-109.56950433055823,250.18517918522548,114.26234092687216&df=true&kiosk=true&l=Coastlines_15m,VIIRS_SNPP_Thermal_Anomalies_375m_Day,VIIRS_SNPP_CorrectedReflectance_TrueColor&lg=true'
+scenario_5_local = 'http://localhost:3000/?v=-243.27471729873875,-101.84591344837116,205.18810596457044,98.79448508453643&df=true&kiosk=true&l=IMERG_Precipitation_Rate,Land_Mask&lg=false'
+scenario_6_local = 'http://localhost:3000/?v=-249.0656215906355,-115.52476681117328,248.89235122405506,107.25955540123256&df=true&kiosk=true&l=Coastlines_15m(opacity=0.71),GHRSST_L4_MUR_Sea_Surface_Temperature(palette=rainbow_1)&lg=true'
+scenario_7_local = 'http://localhost:3000/?v=-250.02535487803547,-114.4840680358938,251.1027632513897,109.71856398138546&df=true&kiosk=true&l=Coastlines_15m,MODIS_Aqua_L3_Land_Surface_Temp_Daily_Day,MODIS_Aqua_CorrectedReflectance_TrueColor(opacity=0.7)&lg=true'
+scenario_8_local = 'http://localhost:3000/?v=-9215416.788865805,-4212995.281243633,9489665.699466601,4155580.686192584&p=arctic&df=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
+
+scenario_1_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-250.17116762774398,-114.67919463709383,250.62557403459047,109.37518092954436&df=true&kiosk=true&l=Coastlines_15m,OrbitTracks_Terra_Descending,MODIS_Terra_CorrectedReflectance_TrueColor&lg=true'
+scenario_2_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-245.94859690671547,-117.45443360228612,249.20837839391078,104.0767339931503&df=true&kiosk=true&l=Coastlines_15m(opacity=0.63),VIIRS_SNPP_DayNightBand_At_Sensor_Radiance&lg=true'
+scenario_3_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-223.38671531740192,-111.23416482365595,252.6798741684782,101.75604370153731&z=4&ics=true&ici=5&icd=10&kiosk=true&df=true&eic=sa&l=Reference_Features_15m,GOES-West_ABI_GeoColor,GOES-East_ABI_GeoColor,VIIRS_SNPP_CorrectedReflectance_TrueColor(opacity=0.61)&lg=true'
+scenario_4_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-250.11417226328018,-109.56950433055823,250.18517918522548,114.26234092687216&df=true&kiosk=true&l=Coastlines_15m,VIIRS_SNPP_Thermal_Anomalies_375m_Day,VIIRS_SNPP_CorrectedReflectance_TrueColor&lg=true'
+scenario_5_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-243.27471729873875,-101.84591344837116,205.18810596457044,98.79448508453643&df=true&kiosk=true&l=IMERG_Precipitation_Rate,Land_Mask&lg=false'
+scenario_6_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-249.0656215906355,-115.52476681117328,248.89235122405506,107.25955540123256&df=true&kiosk=true&l=Coastlines_15m(opacity=0.71),GHRSST_L4_MUR_Sea_Surface_Temperature(palette=rainbow_1)&lg=true'
+scenario_7_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-250.02535487803547,-114.4840680358938,251.1027632513897,109.71856398138546&df=true&kiosk=true&l=Coastlines_15m,MODIS_Aqua_L3_Land_Surface_Temp_Daily_Day,MODIS_Aqua_CorrectedReflectance_TrueColor(opacity=0.7)&lg=true'
+scenario_8_SIT = 'https://worldview.sit.earthdata.nasa.gov/?v=-9215416.788865805,-4212995.281243633,9489665.699466601,4155580.686192584&p=arctic&df=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
+
+
+
+# Replace the following with the 4 pannels you want to test
+urls = [scenario_1_SIT, scenario_2_SIT, scenario_3_SIT, scenario_4_SIT]
+
+options = Options()
+options.headless = False
+
+# Get screen size
+screen_width = int(os.environ.get("WIDTH", 1920))
+screen_height = int(os.environ.get("HEIGHT", 1080))
+
+# Create browser instances
+browsers = [webdriver.Chrome(options=options) for _ in range(4)]
+
+# Set window positions and sizes for each instance
+window_width = screen_width // 2
+window_height = screen_height // 2
+for i, browser in enumerate(browsers):
+    browser.set_window_position(window_width * (i % 2), window_height * (i // 2))
+    browser.set_window_size(window_width, window_height)
+
+# Refresh the pages every 90 seconds
+while True:
+    for browser, url in zip(browsers, urls):
+        browser.get(url)
+    time.sleep(90)
